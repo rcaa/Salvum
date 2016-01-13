9b14a2dd70a35b2501713a0cab96acc68a185f2d
Rodrigo Andrade <rodrigo_cardoso@hotmail.it>
2016-01-13 09:17:00 -0300

    supporting OpenRefine build

--- Salvum/src/br/ufpe/cin/ant/ProjectBuilder.java
+++ Salvum/src/br/ufpe/cin/ant/ProjectBuilder.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.ProjectHelper;
 import org.eclipse.core.runtime.CoreException;
@@ -13,22 +14,29 @@ public class ProjectBuilder {
 	public static void compileProject(Properties p, String commitHash)
 			throws IOException, CoreException {
 
-		 Project project = new Project();
-		 File buildFile = new File(p.getProperty("targetPathDirectory")
-		 + p.getProperty("buildScriptPath"));
-		 project.setUserProperty("ant.file", buildFile.getAbsolutePath());
-		 project.setProperty("java.home", p.getProperty("javahome"));
-		 project.init();
-		 ProjectHelper helper = ProjectHelper.getProjectHelper();
-		 project.addReference("ant.projectHelper", helper);
-		 helper.parse(project, buildFile);
-		 String target = project.getDefaultTarget();
-		 project.executeTarget(target);
-
+		// para OpenRefine
+		removeOldFiles(p);
 		
+		Project project = new Project();
+		File buildFile = new File(p.getProperty("targetPathDirectory")
+				+ p.getProperty("buildScriptPath"));
+		project.setUserProperty("ant.file", buildFile.getAbsolutePath());
+		project.setProperty("java.home", p.getProperty("javahome"));
+		project.init();
+		ProjectHelper helper = ProjectHelper.getProjectHelper();
+		project.addReference("ant.projectHelper", helper);
+		helper.parse(project, buildFile);
+		String target = project.getDefaultTarget();
+		project.executeTarget(target);
+
 		// gitblit
-//		ZipUtil zu = new ZipUtil();
-//		zu.unzip(p.getProperty("zips") + "Archive-" + commitHash + ".zip",
-//				p.getProperty("targetPathDirectory"));
+		// ZipUtil zu = new ZipUtil();
+		// zu.unzip(p.getProperty("zips") + "Archive-" + commitHash + ".zip",
+		// p.getProperty("targetPathDirectory"));
+	}
+
+	private static void removeOldFiles(Properties p) throws IOException {
+		File buildDirectory = new File(p.getProperty("classpath"));
+		FileUtils.cleanDirectory(buildDirectory);
 	}
 }
