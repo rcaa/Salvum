fd143be1510196195bbdaf92661b9ac5b43d7402
Rodrigo Andrade <Rodrigo Andrade>
2016-09-28 15:15:23 -0300

    adding policy and config file for teammates

---
+++ Salvum/policies/teammates.pl
@@ -0,0 +1,2 @@
+teammates.storage.entity.Account {email}, teammates.storage.entity.AdminEmail {addressReceiver,subject,content}, teammates.storage.entity.CourseStudent {registrationKey,email} noflow WriteOperation
+	where WriteOperation = {Log.severe,Log.warning,initUsingAppLogMessage,initAsFailure}
