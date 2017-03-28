fc6df827299b9a9b11b35ecf9565f460830a4c2f
rcaa <rodrigo_cardoso@hotmail.it>
2017-03-28 10:00:20 -0300

    java method signature generator

--- ConfigFileGenerator/src/methods/ClassMethod.java
+++ ConfigFileGenerator/src/methods/ClassMethod.java
@@ -14,30 +14,45 @@ public class ClassMethod {
 		Method[] methods = t.getClass().getDeclaredMethods();
 		for (int i = 0; i < methods.length; i++) {
 			Class<?>[] parameterTypes = methods[i].getParameterTypes();
-			String fullMethodName = methods[i].getDeclaringClass().getName() + "." + methods[i].getName() + "(";
-			for (Class<?> paramType : parameterTypes) {
-				String paramTypeNameWithBars = paramType.getName().replace('.','/');
-				if (paramType.isArray() || paramType.isPrimitive()) {
-					fullMethodName = fullMethodName + paramTypeNameWithBars;
-				} else {
-					fullMethodName = fullMethodName + "L" + paramTypeNameWithBars + ";";
-				}
-			}
+			String fullMethodName = methods[i].getDeclaringClass().getName()
+					+ "." + methods[i].getName() + "(";
+			fullMethodName = formatParameters(parameterTypes, fullMethodName);
 			fullMethodName = fullMethodName + ")";
-			Class<?> returnType = methods[i].getReturnType();
-			String returningTypeWithBars = returnType.getName().replace('.', '/');
-			if (returningTypeWithBars.equals("void")) {
-				fullMethodName = fullMethodName + "V";
-			} else if (returnType.isArray() || returnType.isPrimitive()) {
-				fullMethodName = fullMethodName + returningTypeWithBars;
-			} else {
-				fullMethodName = fullMethodName + "L" + returningTypeWithBars + ";";
-			}
+			fullMethodName = formatReturningType(methods, i, fullMethodName);
 			System.out.println(fullMethodName);
 		}
-//		Constructor[] constructors = tClass.getDeclaredConstructors();
-//		for (int i = 0; i < constructors.length; i++) {
-//			System.out.println(constructors[i]);
-//		}
+		// Constructor[] constructors = tClass.getDeclaredConstructors();
+		// for (int i = 0; i < constructors.length; i++) {
+		// System.out.println(constructors[i]);
+		// }
+	}
+
+	private static String formatReturningType(Method[] methods, int i,
+			String fullMethodName) {
+		Class<?> returnType = methods[i].getReturnType();
+		String returningTypeWithBars = returnType.getName().replace('.', '/');
+		if (returningTypeWithBars.equals("void")) {
+			fullMethodName = fullMethodName + "V";
+		} else if (returnType.isArray() || returnType.isPrimitive()) {
+			fullMethodName = fullMethodName + returningTypeWithBars;
+		} else {
+			fullMethodName = fullMethodName + "L" + returningTypeWithBars + ";";
+		}
+		return fullMethodName;
+	}
+
+	private static String formatParameters(Class<?>[] parameterTypes,
+			String fullMethodName) {
+		for (Class<?> paramType : parameterTypes) {
+			String paramTypeNameWithBars = paramType.getName()
+					.replace('.', '/');
+			if (paramType.isArray() || paramType.isPrimitive()) {
+				fullMethodName = fullMethodName + paramTypeNameWithBars;
+			} else {
+				fullMethodName = fullMethodName + "L" + paramTypeNameWithBars
+						+ ";";
+			}
+		}
+		return fullMethodName;
 	}
 }
